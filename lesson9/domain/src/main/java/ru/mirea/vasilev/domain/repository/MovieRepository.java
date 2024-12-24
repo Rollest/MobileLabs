b9104a4420b54aef869344568a3934508f8c585b
Maksim Vasilev <104532816+Rollest@users.noreply.github.com>
2024-12-24 21:25:16 +0300

    Delete lesson9 directory

--- lesson9/domain/src/main/java/ru/mirea/vasilev/domain/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.vasilev.domain.repository;
-
-import ru.mirea.vasilev.domain.models.Movie;
-
-public interface MovieRepository {
-    public boolean saveMovie(Movie movie);
-    public Movie getMovie();
-}
